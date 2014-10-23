4bfd2ae7742638b6358dc43a6ef245a9d7a30937
沈镛俊 <928467259@qq.com>
2014-10-23 10:06:46 +0800

    修复冲突

--- Meizhi/ZhanshishejiVC.m
+++ Meizhi/ZhanshishejiVC.m
@@ -80,7 +80,7 @@
     NSString *s = [[NSBundle mainBundle] pathForResource:@"zhanshi" ofType:@"mp4"];
     NSURL *url = [NSURL fileURLWithPath:s];
     theMovie= [[MPMoviePlayerController alloc] initWithContentURL:url];
-    theMovie.
+    
     
     theMovie.view.frame = CGRectMake(0, 0, 1024, 768);
     [self.viewMovie addSubview:theMovie.view];
